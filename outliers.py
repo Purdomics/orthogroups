5168cf5ef06a18e4b81a18023acd6ad5ee439317
Michael Gribskov <mgribsko@gmail.com>
2024-02-20 11:01:30 -0500

    outliers.py - calculates normalized data

--- outliers.py
+++ outliers.py
@@ -16,33 +16,33 @@ group[i][j][k] are the individual matching sequences
 18 February 2024     gribskov
 ================================================================================================="""
 import sys
-from math import floor, ceil
-# from scipy.stats import trim_mean, tstd
+# from math import floor, ceil
+from scipy.stats.mstats import trimmed_mean, trimmed_std
+# from scipy import stats
 import numpy as np
-import statistics as stat
 from orthogroups import Orthogroup
 
 
-def trimmed_stats(data, norm, proportion, ignore_zero=True):
+def trimmed_stats(og, proportion, ignore_zero=True):
     """---------------------------------------------------------------------------------------------
     return the trimmed mean and standard deviation. proportion defines how much data is omitted on
     each side (proportion/2). After sorting the first index used is the first >= proportion/2 and
     the last is the one <= 1.0 - proportion/2
 
-    :param data: list           Othogroup.group data, rows are orthogroups
-    :param norm:
+    :param og: list             Othogroup object, rows are orthogroups
     :param proportion:          proportion to trim proportion/2 trimmed on each side
-    :param ignore_zero: bool    if True, zeroes are omitted otherwise they are treated as values
-                                note that the list
-    :return: float, float       trimmed mean and standard deviation
+    :param ignore_zero: bool    NOT IMPLEMENTED. if True, zeroes are omitted otherwise they are
+                                treated as values
+    :return: float, float       Z normalized data
     ---------------------------------------------------------------------------------------------"""
-    use_zero = not ignore_zero
-    cutmin = proportion / 2
-    cutmax = 1.0 - proportion / 2
+    # use_zero = not ignore_zero
+    cut = proportion / 2
 
-    nil = 0
-    if ignore_zero:
-        nil = np.nan
+    # nil = 0
+    # if ignore_zero:
+    #     nil = np.nan
+    data = og.group
+    norm = np.full([len(data), len(og.proteome)], np.nan)
 
     # make count vector from original data, and store in norm using zero or NaN depending
     # on the state of ignore_zero
@@ -51,26 +51,17 @@ def trimmed_stats(data, norm, proportion, ignore_zero=True):
         nspecie = len(data[g])
         norm[g] = [len(data[g][s]) for s in range(nspecie)]
 
-    if ignore_zero:
-        norm[norm == 0] = np.nan
+    # if ignore_zero:
+    #     norm[norm == 0] = np.nan
 
-    # mean and standard deviation ignoring NaN
-    ave = np.nanmean(norm, 1)
-    std = np.nanstd(norm, 1)
-    std[std==0] = 1
+    # mean and standard deviation, values are trimmed until index+1 >= n*cut
+    # for n=12 and cut = .125, two values are cut off
 
-    for g in range(ngroup):
-        aave = ave[g]
-        astd = std[g]
-        norm[g] = (norm[g] - aave) /astd
+    ave = trimmed_mean(norm, limits=(cut, cut), inclusive=(False, False), axis=1)
+    std = trimmed_std(norm, limits=(cut, cut), inclusive=(False, False), axis=1)
+    std[std == 0] = 1
 
-    # start = floor(n * cutmin)
-    # stop = ceil(n * cutmax)
-    # print(f'n: {n}\tstart: {start}\tstop: {stop}')
-
-
-
-    return ave, std
+    return ((norm.T - ave) / std).T
 
 
 # ==================================================================================================
@@ -87,15 +78,14 @@ if __name__ == '__main__':
     print(f'{n_seq} sequences read')
 
     # make a trimmed list of genome names (part up to first underline)
-    organism = []
-    for p in og.proteome:
-        field = p.split('_')
-        organism.append(field[0])
+    organism = {}
+    for p in range(len(og.proteome)):
+        field = og.proteome[p].split('_')
+        organism[field[0]] = p
 
     # normalize the count data so that each row is a standard normal deviate, using trimmed mean
     # and STD dev.
-    # transformed = np.zeros(shape=(len(og.group), len(og.proteome)))
-    transformed = np.full([len(og.group), len(og.proteome)], np.nan)
-    stats = trimmed_stats(og.group, transformed, 0.25, ignore_zero=True)
+
+    z = trimmed_stats(og, 0.5, ignore_zero=True)
 
     exit(0)
